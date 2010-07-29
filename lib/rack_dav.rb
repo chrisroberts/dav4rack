11ae6f5bfc56d3fb5b6bbb4c7be44fd04737ea85
chrisroberts <chrisroberts@colortechnology.com>
2010-07-29 08:29:46 -0700

    Don't load FileResource automatically

--- lib/rack_dav.rb
+++ lib/rack_dav.rb
@@ -8,7 +8,6 @@ require 'rack'
 require 'rack_dav/builder_namespace'
 require 'rack_dav/http_status'
 require 'rack_dav/resource'
-require 'rack_dav/file_resource'
 require 'rack_dav/handler'
 require 'rack_dav/controller'
 
