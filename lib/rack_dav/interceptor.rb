d42d9b7a211057e0944944c2927e11877c64f715
chrisroberts <chrisroberts@colortechnology.com>
2010-07-30 16:36:36 -0700

    Grab more request types

--- lib/rack_dav/interceptor.rb
+++ lib/rack_dav/interceptor.rb
@@ -6,12 +6,12 @@ module RackDAV
       @args = args
       @app = app
     end
-    
+
     def call(env)
       path = env['REQUEST_PATH'].downcase
       method = env['REQUEST_METHOD']
       app = nil
-      if(@roots.detect{|x| path =~ /^#{Regexp.escape(x.downcase)}\/?/}.nil? && ['OPTIONS', 'PROPFIND'].include?(method))
+      if(@roots.detect{|x| path =~ /^#{Regexp.escape(x.downcase)}\/?/}.nil? && %w(OPTIONS PUT PROPFIND PROPPATCH MKCOL COPY MOVE LOCK UNLOCK).include?(method))
         puts "Intercepting with method: #{method} for path: #{path}"
         app = RackDAV::Handler.new(:resource_class => InterceptorResource, :mappings => @args[:mappings])
       end
