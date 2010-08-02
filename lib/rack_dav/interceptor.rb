029521cc651ca3aebfc3d565710d6258282bb89d
chrisroberts <chrisroberts@colortechnology.com>
2010-08-02 13:36:57 -0700

    Use PATH_INFO

--- lib/rack_dav/interceptor.rb
+++ lib/rack_dav/interceptor.rb
@@ -8,11 +8,10 @@ module RackDAV
     end
 
     def call(env)
-      path = env['REQUEST_PATH'].downcase
-      method = env['REQUEST_METHOD']
+      path = env['PATH_INFO'].downcase
+      method = env['REQUEST_METHOD'].upcase
       app = nil
       if(@roots.detect{|x| path =~ /^#{Regexp.escape(x.downcase)}\/?/}.nil? && %w(OPTIONS PUT PROPFIND PROPPATCH MKCOL COPY MOVE LOCK UNLOCK).include?(method))
-        puts "Intercepting with method: #{method} for path: #{path}"
         app = RackDAV::Handler.new(:resource_class => InterceptorResource, :mappings => @args[:mappings])
       end
       app ? app.call(env) : @app.call(env)
