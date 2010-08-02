e4d71b4c10895d62873df33857b4017d6e7bf25d
chrisroberts <chrisroberts@colortechnology.com>
2010-08-02 13:36:13 -0700

    Set status code if available. Force all header values to strings.

--- lib/rack_dav/handler.rb
+++ lib/rack_dav/handler.rb
@@ -14,13 +14,12 @@ module RackDAV
     def call(env)
 
       request = Rack::Request.new(env)
-      pp request
       response = Rack::Response.new
-
+      
       begin
         controller = Controller.new(request, response, @options.dup)
         res = controller.send(request.request_method.downcase)
-        response.status = res.code if res.is_a?(HTTPStatus::Status)
+        response.status = res.code if res.respond_to?(:code)
       rescue HTTPStatus::Status => status
         response.status = status.code
       end
@@ -30,8 +29,13 @@ module RackDAV
 
       response.body = [response.body] if not response.body.respond_to? :each
       response.status = response.status ? response.status.to_i : 200
+      response['Content-Length'] = response.body.to_s.length unless response['Content-Length']
+      response.headers.each_pair{|k,v| response[k] = v.to_s}
+
+      # Apache wants the body dealt with, so just read it and junk it
+      buf = true
+      buf = request.body.read(8192) while buf
       
-      puts response.body
       response.finish
     end
     
