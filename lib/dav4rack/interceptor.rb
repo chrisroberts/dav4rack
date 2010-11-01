f2902fe37b95b1a57d666be2c3e79c405e4767bc
chrisroberts <chrisroberts@colortechnology.com>
2010-11-01 08:52:22 -0700

    Provide option to ignore HTTP request methods

--- lib/dav4rack/interceptor.rb
+++ lib/dav4rack/interceptor.rb
@@ -5,13 +5,15 @@ module DAV4Rack
       @roots = args[:mappings].keys
       @args = args
       @app = app
+      @intercept_methods = %w(OPTIONS PROPFIND PROPPATCH MKCOL COPY MOVE LOCK UNLOCK)
+      @intercept_methods -= args[:ignore_methods] if args[:ignore_methods]
     end
 
     def call(env)
       path = env['PATH_INFO'].downcase
       method = env['REQUEST_METHOD'].upcase
       app = nil
-      if(@roots.detect{|x| path =~ /^#{Regexp.escape(x.downcase)}\/?/}.nil? && %w(OPTIONS PUT PROPFIND PROPPATCH MKCOL COPY MOVE LOCK UNLOCK).include?(method))
+      if(@roots.detect{|x| path =~ /^#{Regexp.escape(x.downcase)}\/?/}.nil? && @intercept_methods.include?(method))
         app = DAV4Rack::Handler.new(:resource_class => InterceptorResource, :mappings => @args[:mappings], :log_to => @args[:log_to])
       end
       app ? app.call(env) : @app.call(env)
