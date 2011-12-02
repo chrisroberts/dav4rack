26fbf51a4ae830e09a1357934e102e6a1cf2d3d4
exabugs <exabugs@gmail.com>
2011-12-02 15:36:13 +0900

    Add MongoResource.

---
+++ lib/dav4rack/mongo_resource.rb
@@ -0,0 +1,340 @@
+# coding: utf-8
+require 'mime/types'
+
+module DAV4Rack
+
+  class MongoResource < DAV4Rack::Resource
+
+#    @@logger = Rails.logger
+
+    def initialize(public_path, path, request, response, options)
+      # 'ASCII-8BIT'で渡される場合があるので'UTF-8'を指定しておく
+      _force_encoding!(public_path)
+      _force_encoding!(path)
+      super(public_path, path, request, response, options)
+      @filesystem = Mongo::GridFileSystem.new(Mongoid.database)
+      @collection = Mongoid.database.collection('fs.files')
+      if options[:bson]
+        @bson = options[:bson]
+      elsif path.length <= 1
+        # ルートの場合 (''の場合と'/'の場合がある)
+        @bson = {'filename' => root + '/'}
+      else
+        # ファイルかディレクトリが、パラメータだけでは判断できない。ので \/? が必要。
+        # だから、ディレクトリと同名のファイルは、作成できない。
+        @bson = @collection.find_one({:filename => /^#{Regexp.escape(file_path)}\/?$/}) rescue nil
+      end
+    end
+
+    def child(bson)
+      path = remove(bson['filename'], root)
+      public_path = @options[:root_uri_path] + path
+      @options[:bson] = bson
+      self.class.new(public_path, path, @request, @response, @options)
+    end
+
+    # If this is a collection, return the child resources.
+    def children
+#     Dir[file_path + '/*'].map do |path|
+#       child File.basename(path)
+#     end
+      @collection.find({:filename => /^#{Regexp.escape(@bson['filename'])}[^\/]+\/?$/}).map do |bson|
+        child bson
+      end
+    end
+
+    # Is this resource a collection?
+    def collection?
+#     File.directory?(file_path)
+      @bson && _collection?(@bson['filename'])
+    end
+
+    # Does this recource exist?
+    def exist?
+#     File.exist?(file_path)
+      @bson
+    end
+
+    # Return the creation time.
+    def creation_date
+#     stat.ctime
+      @bson['uploadDate'] || Date.new
+    end
+
+    # Return the time of last modification.
+    def last_modified
+#     stat.mtime
+      @bson['uploadDate'] || Date.new
+    end
+
+    # Set the time of last modification.
+    def last_modified=(time)
+#      File.utime(Time.now, time, file_path)
+    end
+
+    # Return an Etag, an unique hash value for this resource.
+    def etag
+#     sprintf('%x-%x-%x', stat.ino, stat.size, stat.mtime.to_i)
+      @bson['_id'].to_s
+    end
+
+    # Return the mime type of this resource.
+    def content_type
+#     if stat.directory?
+#       "text/html"
+#     else 
+#       mime_type(file_path, DefaultMimeTypes)
+#     end
+      @bson['contentType'] || "text/html"
+    end
+
+    # Return the size in bytes for this resource.
+    def content_length
+#     stat.size
+      @bson['length'] || 0
+    end
+
+    # HTTP GET request.
+    #
+    # Write the content of the resource to the response.body.
+    def get(request, response)
+      raise NotFound unless exist?
+#     if stat.directory?
+#       response.body = ""
+#       Rack::Directory.new(root).call(request.env)[2].each do |line|
+#         response.body << line
+#       end
+#       response['Content-Length'] = response.body.size.to_s
+#     else
+#       file = Rack::File.new(root)
+#       response.body = file
+#     end
+      if collection?
+        response.body = "<html>"
+        response.body << "<h2>" + file_path.html_safe + "</h2>"
+        children.each do |child|
+          name = child.file_path.html_safe
+          path = child.public_path
+          response.body << "<a href='" + path + "'>" + name + "</a>"
+          response.body << "</br>"
+        end
+        response.body << "</html>"
+        response['Content-Length'] = response.body.size.to_s
+        response['Content-Type'] = 'text/html'
+      else
+        @filesystem.open(file_path, 'r') do |f|
+          response.body = f
+          response['Content-Type'] = @bson['contentType']
+        end
+      end
+
+    end
+
+    # HTTP PUT request.
+    #
+    # Save the content of the request.body.
+    def put(request, response)
+      write(request.body)
+      Created
+    end
+
+    # HTTP POST request.
+    #
+    # Usually forbidden.
+    def post(request, response)
+      raise HTTPStatus::Forbidden
+    end
+
+    # HTTP DELETE request.
+    #
+    # Delete this resource.
+    def delete
+#     if stat.directory?
+#       FileUtils.rm_rf(file_path)
+#     else
+#       File.unlink(file_path)
+#     end
+      if collection?
+        @collection.find({:filename => /^#{Regexp.escape(@bson['filename'])}/}).each do |bson|
+          @collection.remove(bson)
+        end
+      else
+        @collection.remove(@bson)
+      end
+      NoContent
+    end
+
+    # HTTP COPY request.
+    #
+    # Copy this resource to given destination resource.
+    def copy(dest, overwrite = false)
+#     if(dest.path == path)
+#       Conflict
+#     elsif(stat.directory?)
+#       dest.make_collection
+#       FileUtils.cp_r("#{file_path}/.", "#{dest.send(:file_path)}/")
+#       OK
+#     else
+#       exists = File.exists?(file_path)
+#       if(exists && !overwrite)
+#         PreconditionFailed
+#       else
+#         open(file_path, "rb") do |file|
+#           dest.write(file)
+#         end
+#         exists ? NoContent : Created
+#       end
+#     end
+
+      # ディレクトリなら末尾に「/」をつける。
+      # (dstにもともと「/」が付いているかどうか、クライアントに依存している)
+      # CarotDAV : 「/」が付いていない
+      # TeamFile : 「/」が付いている
+      dest.collection! if collection?
+
+      src = @bson['filename']
+      dst = dest.file_path
+      exists = nil
+
+      @collection.find({:filename => /^#{Regexp.escape(src)}/}).each do |bson|
+        src_name = bson['filename']
+        dst_name = dst + src_name.slice(src.length, src_name.length)
+
+        exists = @collection.find_one({:filename => dst_name}) rescue nil
+
+        return PreconditionFailed if (exists && !overwrite && !collection?)
+
+        @filesystem.open(src_name, "r") do |src|
+        @filesystem.open(dst_name, "w") do |dst|
+          dst.write(src) if src.file_length > 0
+        end
+        end
+
+        @collection.remove(exists) if exists
+      end
+
+      collection? ? OK : (exists ? NoContent : Created)
+    end
+
+    # HTTP MOVE request.
+    #
+    # Move this resource to given destination resource.
+    def move(dest, overwrite = false)
+
+      # ディレクトリなら末尾に「/」をつける。
+      # (dstにもともと「/」が付いているかどうか、クライアントに依存している)
+      # CarotDAV : 「/」が付いていない
+      # TeamFile : 「/」が付いている
+      dest.collection! if collection?
+
+      src = @bson['filename']
+      dst = dest.file_path
+      exists = nil
+
+      @collection.find({:filename => /^#{Regexp.escape(src)}/}).each do |bson|
+        src_name = bson['filename']
+        dst_name = dst + src_name.slice(src.length, src_name.length)
+
+        exists = @collection.find_one({:filename => dst_name}) rescue nil
+
+        bson['filename'] = dst_name
+        @collection.save(bson)
+
+        @collection.remove(exists) if exists
+      end
+
+      collection? ? OK : (exists ? NoContent : Created)
+    end
+
+    # HTTP MKCOL request.
+    #
+    # Create this resource as collection.
+    def make_collection
+#     Dir.mkdir(file_path)
+#     Created
+
+      # ディレクトリなら末尾に「/」をつける。
+      # (dstにもともと「/」が付いているかどうか、クライアントに依存している)
+      # CarotDAV : 「/」が付いていない
+      # TeamFile : 「/」が付いている
+      collection!
+
+      bson = @collection.find_one({:filename => file_path}) rescue nil
+
+      # 0バイトのファイルを作成しディレクトリの代わりとする
+      @filesystem.open(file_path, "w") { |f| } if !bson
+ 
+#      @@logger.error('make_collection : ' + file_path)
+
+      Created
+    end
+
+    # Write to this resource from given IO.
+    def write(io)
+#     tempfile = "#{file_path}.#{Process.pid}.#{object_id}"
+#     open(tempfile, "wb") do |file|
+#       while part = io.read(8192)
+#         file << part
+#       end
+#     end
+#     File.rename(tempfile, file_path)
+#     ensure
+#       File.unlink(tempfile) rescue nil
+
+      # 同名のファイルができないように
+      bson = @collection.find_one({:filename => file_path}) rescue nil
+
+      @filesystem.open(file_path, "w", :content_type => _content_type(file_path)) { |f| f.write(io) }
+
+      # 同名のファイルができないように
+      @collection.remove(bson) if bson
+
+    end
+
+    protected
+
+    def file_path
+      root + path
+    end
+
+    # ファイル名の末尾に「/」を付加してディレクトリ（コレクション）とする
+    def collection!
+      path << '/' if !_collection?(path)
+    end
+
+    private
+
+    def _content_type(filename)
+      MIME::Types.type_for(filename).first.to_s || 'text/html'
+    end
+
+    def authenticate(user, pass)
+      if(options[:username])
+        options[:username] == user && options[:password] == pass
+      else
+        true
+      end
+    end
+
+    # path1の先頭からpath2を取り除く
+    def remove(path1, path2)
+      path1.slice(path2.length, path1.length)
+    end
+
+    def root
+      @options[:root]
+    end
+
+    # ファイル名の末尾が「/」のファイルをディレクトリ（コレクション）とする
+    def _collection?(path)
+      path && path[-1].chr == '/'
+    end
+
+    # 'ASCII-8BIT'で渡される場合があるので'UTF-8'を指定しておく
+    def _force_encoding!(str)
+      str.force_encoding('UTF-8')
+    end
+
+  end
+
+end
---
+++ spec/mongo_spec.rb
@@ -0,0 +1,312 @@
+$:.unshift(File.expand_path(File.dirname(__FILE__) + '/../lib'))
+
+require 'rubygems'
+require 'dav4rack'
+require 'fileutils'
+require 'nokogiri'
+require 'dav4rack/mongo_resource'
+require 'mongo'
+require 'mongoid'
+
+describe DAV4Rack::Handler do
+
+  before do
+#   FileUtils.mkdir(DOC_ROOT) unless File.exists?(DOC_ROOT)
+#   @controller = DAV4Rack::Handler.new(:root => DOC_ROOT)
+    Mongoid.database = Mongo::Connection.new("localhost").db("test")
+    @controller = DAV4Rack::Handler.new(:root => '/webdav', :resource_class => ::DAV4Rack::MongoResource, :root_uri_path => '')
+  end
+
+  after do
+#   FileUtils.rm_rf(DOC_ROOT) if File.exists?(DOC_ROOT)
+    Mongoid.database.collection('fs.files').drop
+  end
+  
+  attr_reader :response
+  
+  def request(method, uri, options={})
+    options = {
+      'HTTP_HOST' => 'localhost',
+      'REMOTE_USER' => 'user'
+    }.merge(options)
+    request = Rack::MockRequest.new(@controller)
+    @response = request.request(method, uri, options)
+  end
+
+  METHODS.each do |method|
+    define_method(method.downcase) do |*args|
+      request(method, *args)
+    end
+  end  
+  
+  def render(root_type)
+    raise ArgumentError.new 'Expecting block' unless block_given?
+    doc = Nokogiri::XML::Builder.new do |xml_base|
+      xml_base.send(root_type.to_s, 'xmlns:D' => 'D:') do
+        xml_base.parent.namespace = xml_base.parent.namespace_definitions.first
+        xml = xml_base['D']
+        yield xml
+      end
+    end
+    doc.to_xml
+  end
+ 
+  def url_escape(string)
+    URI.escape(string)
+  end
+  
+  def response_xml
+    Nokogiri.XML(@response.body)
+  end
+  
+  def multistatus_response(pattern)
+    @response.should be_multi_status
+    response_xml.xpath('//D:multistatus/D:response', response_xml.root.namespaces).should_not be_empty
+    response_xml.xpath("//D:multistatus/D:response#{pattern}", response_xml.root.namespaces)
+  end
+
+  def multi_status_created
+    response_xml.xpath('//D:multistatus/D:response/D:status').should_not be_empty
+    response_xml.xpath('//D:multistatus/D:response/D:status').text.should =~ /Created/
+  end
+  
+  def multi_status_ok
+    response_xml.xpath('//D:multistatus/D:response/D:status').should_not be_empty
+    response_xml.xpath('//D:multistatus/D:response/D:status').text.should =~ /OK/
+  end
+  
+  def multi_status_no_content
+    response_xml.xpath('//D:multistatus/D:response/D:status').should_not be_empty
+    response_xml.xpath('//D:multistatus/D:response/D:status').text.should =~ /No Content/
+  end
+  
+  def propfind_xml(*props)
+    render(:propfind) do |xml|
+      xml.prop do
+        props.each do |prop|
+        xml.send(prop.to_sym)
+        end
+      end
+    end
+  end
+  
+  it 'should return all options' do
+    options('/').should be_ok
+    
+    METHODS.each do |method|
+      response.headers['allow'].should include(method)
+    end
+  end
+  
+  it 'should return headers' do
+    put('/test.html', :input => '<html/>').should be_created
+    head('/test.html').should be_ok
+    
+    response.headers['etag'].should_not be_nil
+    response.headers['content-type'].should match(/html/)
+    response.headers['last-modified'].should_not be_nil
+  end
+  
+  it 'should not find a nonexistent resource' do
+    get('/not_found').should be_not_found
+  end
+  
+  it 'should not allow directory traversal' do
+    get('/../htdocs').should be_forbidden
+  end
+  
+  it 'should create a resource and allow its retrieval' do
+    put('/test', :input => 'body').should be_created
+    get('/test').should be_ok
+    response.body.should == 'body'
+  end
+
+  it 'should return an absolute url after a put request' do
+    put('/test', :input => 'body').should be_created
+    response['location'].should =~ /http:\/\/localhost(:\d+)?\/test/
+  end
+  
+  it 'should create and find a url with escaped characters' do
+    put(url_escape('/a b'), :input => 'body').should be_created
+    get(url_escape('/a b')).should be_ok
+    response.body.should == 'body'
+  end
+  
+  it 'should delete a single resource' do
+    put('/test', :input => 'body').should be_created
+    delete('/test').should be_no_content
+  end
+  
+  it 'should delete recursively' do
+    mkcol('/folder').should be_created
+    put('/folder/a', :input => 'body').should be_created
+    put('/folder/b', :input => 'body').should be_created
+    
+    delete('/folder').should be_no_content
+    get('/folder').should be_not_found
+    get('/folder/a').should be_not_found
+    get('/folder/b').should be_not_found
+  end
+
+  it 'should not allow copy to another domain' do
+    put('/test', :input => 'body').should be_created
+    copy('http://localhost/', 'HTTP_DESTINATION' => 'http://another/').should be_bad_gateway
+  end
+
+  it 'should not allow copy to the same resource' do
+    put('/test', :input => 'body').should be_created
+    copy('/test', 'HTTP_DESTINATION' => '/test').should be_forbidden
+  end
+
+  it 'should copy a single resource' do
+    put('/test', :input => 'body').should be_created
+    copy('/test', 'HTTP_DESTINATION' => '/copy')
+    multi_status_created.should eq true
+    get('/copy').body.should == 'body'
+  end
+
+  it 'should copy a resource with escaped characters' do
+    put(url_escape('/a b'), :input => 'body').should be_created
+    copy(url_escape('/a b'), 'HTTP_DESTINATION' => url_escape('/a c'))
+    multi_status_created.should eq true
+    get(url_escape('/a c')).should be_ok
+    response.body.should == 'body'
+  end
+  
+  it 'should deny a copy without overwrite' do
+    put('/test', :input => 'body').should be_created
+    put('/copy', :input => 'copy').should be_created
+    copy('/test', 'HTTP_DESTINATION' => '/copy', 'HTTP_OVERWRITE' => 'F')
+    
+    multistatus_response('/D:href').first.text.should =~ /http:\/\/localhost(:\d+)?\/test/
+    multistatus_response('/D:status').first.text.should match(/412 Precondition Failed/)
+    
+    get('/copy').body.should == 'copy'
+  end
+  
+  it 'should allow a copy with overwrite' do
+    put('/test', :input => 'body').should be_created
+    put('/copy', :input => 'copy').should be_created
+    copy('/test', 'HTTP_DESTINATION' => '/copy', 'HTTP_OVERWRITE' => 'T')
+    multi_status_no_content.should eq true
+    get('/copy').body.should == 'body'
+  end
+  
+  it 'should copy a collection' do  
+    mkcol('/folder').should be_created
+    copy('/folder', 'HTTP_DESTINATION' => '/copy')
+    multi_status_ok.should eq true
+    propfind('/copy', :input => propfind_xml(:resourcetype))
+    multistatus_response('/D:propstat/D:prop/D:resourcetype/D:collection').should_not be_empty
+  end
+
+  it 'should copy a collection resursively' do
+    mkcol('/folder').should be_created
+    put('/folder/a', :input => 'A').should be_created
+    put('/folder/b', :input => 'B').should be_created
+    
+    copy('/folder', 'HTTP_DESTINATION' => '/copy')
+    multi_status_ok.should eq true
+    propfind('/copy', :input => propfind_xml(:resourcetype))
+    multistatus_response('/D:propstat/D:prop/D:resourcetype/D:collection').should_not be_empty
+    get('/copy/a').body.should == 'A'
+    get('/copy/b').body.should == 'B'
+  end
+  
+  it 'should move a collection recursively' do
+    mkcol('/folder').should be_created
+    put('/folder/a', :input => 'A').should be_created
+    put('/folder/b', :input => 'B').should be_created
+    
+    move('/folder', 'HTTP_DESTINATION' => '/move')
+    multi_status_ok.should eq true
+    propfind('/move', :input => propfind_xml(:resourcetype))
+    multistatus_response('/D:propstat/D:prop/D:resourcetype/D:collection').should_not be_empty    
+    
+    get('/move/a').body.should == 'A'
+    get('/move/b').body.should == 'B'
+    get('/folder/a').should be_not_found
+    get('/folder/b').should be_not_found
+  end
+  
+  it 'should create a collection' do
+    mkcol('/folder').should be_created
+    propfind('/folder', :input => propfind_xml(:resourcetype))
+    multistatus_response('/D:propstat/D:prop/D:resourcetype/D:collection').should_not be_empty
+  end
+  
+  it 'should return full urls after creating a collection' do
+    mkcol('/folder').should be_created
+    propfind('/folder', :input => propfind_xml(:resourcetype))
+    multistatus_response('/D:propstat/D:prop/D:resourcetype/D:collection').should_not be_empty
+    multistatus_response('/D:href').first.text.should =~ /http:\/\/localhost(:\d+)?\/folder/
+  end
+  
+  it 'should not find properties for nonexistent resources' do
+    propfind('/non').should be_not_found
+  end
+  
+  it 'should find all properties' do
+    xml = render(:propfind) do |xml|
+      xml.allprop
+    end
+    
+    propfind('http://localhost/', :input => xml)
+    
+    multistatus_response('/D:href').first.text.strip.should =~ /http:\/\/localhost(:\d+)?\//
+
+    props = %w(creationdate displayname getlastmodified getetag resourcetype getcontenttype getcontentlength)
+    props.each do |prop|
+      multistatus_response("/D:propstat/D:prop/D:#{prop}").should_not be_empty
+    end
+  end
+  
+  it 'should find named properties' do
+    put('/test.html', :input => '<html/>').should be_created
+    propfind('/test.html', :input => propfind_xml(:getcontenttype, :getcontentlength))
+   
+    multistatus_response('/D:propstat/D:prop/D:getcontenttype').first.text.should == 'text/html'
+    multistatus_response('/D:propstat/D:prop/D:getcontentlength').first.text.should == '7'
+  end
+
+  it 'should lock a resource' do
+    put('/test', :input => 'body').should be_created
+    
+    xml = render(:lockinfo) do |xml|
+      xml.lockscope { xml.exclusive }
+      xml.locktype { xml.write }
+      xml.owner { xml.href "http://test.de/" }
+    end
+
+    lock('/test', :input => xml)
+    
+    response.should be_ok
+    
+    match = lambda do |pattern|
+      response_xml.xpath "/D:prop/D:lockdiscovery/D:activelock#{pattern}"
+    end
+    
+    match[''].should_not be_empty
+
+    match['/D:locktype'].should_not be_empty
+    match['/D:lockscope'].should_not be_empty
+    match['/D:depth'].should_not be_empty
+    match['/D:timeout'].should_not be_empty
+    match['/D:locktoken'].should_not be_empty
+    match['/D:owner'].should_not be_empty
+  end
+  
+  context "when mapping a path" do
+    
+    before do
+#     @controller = DAV4Rack::Handler.new(:root => DOC_ROOT, :root_uri_path => '/webdav/')
+      @controller = DAV4Rack::Handler.new(:root => '/webdav', :resource_class => ::DAV4Rack::MongoResource, :root_uri_path => '/webdav')
+    end
+    
+    it "should return correct urls" do
+      # FIXME: a put to '/test' works, too -- should it?
+      put('/webdav/test', :input => 'body').should be_created
+      response.headers['location'].should =~ /http:\/\/localhost(:\d+)?\/webdav\/test/
+    end
+  end
+end
