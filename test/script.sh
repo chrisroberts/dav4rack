0cc402730f8f3baa164316f99307940efa3d1378
Chris Roberts <chrisroberts.code@gmail.com>
2012-03-11 19:43:39 -0700

    Provide proper exit codes

--- test/script.sh
+++ test/script.sh
@@ -3,12 +3,27 @@
 # Run buitin spec tests
 bundle exec rake test
 
-# Run litmus test
-bundle exec dav4rack --root /tmp/dav-file-store
+if [ $? -ne 0 ] ; then
+  echo "*** Specs failed to properly complete"
+  exit 1
+fi
 
-DAV_PID = $?
+# Run litmus test
+bundle exec dav4rack --root /tmp/dav-file-store &
+
+# Allow time for dav4rack to get started
+sleep 3
+
+DAV_PID=$?
 
 cd /tmp/litmus/litmus-0.13 
 make URL=http://localhost:3000/ check
 
+LITMUS=$?
+
 kill $DAV_PID
+
+if [ $? -ne 0 ] ; then
+  echo "*** Litmus failed to properly complete"
+  exit 1
+fi
