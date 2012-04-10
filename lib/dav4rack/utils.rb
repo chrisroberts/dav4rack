ab3404cd5b4315394ef37b45376687715cff9dd8
Alex Zepeda <alex@inferiorhumanorgans.com>
2012-04-10 14:29:46 -0700

    Include attributes in the element hash.

--- lib/dav4rack/utils.rb
+++ lib/dav4rack/utils.rb
@@ -5,13 +5,21 @@ module DAV4Rack
       ns = element.namespace
       prefix = ns.nil? ? nil : ns.prefix
       href = ns.nil? ? nil : ns.href
-      {:name => element.name, :ns_href => href, :children => element.children.collect{|e| to_element_hash(e)}}
+      {:name => element.name, :ns_href => href, :children => element.children.collect{|e| to_element_hash(e) if e.element? }.compact, :attributes => attributes_hash(element)}
     end
 
     def to_element_key(element)
       ns = element.namespace
       href = ns.nil? ? nil : ns.href
       "#{href}!!#{element.name}"
+    end
+
+    private
+    def attributes_hash(node)
+      node.attributes.inject({}) do |ret, (key,attr)|
+        ret[attr.name]=attr.value
+        ret
+      end
     end
   end
 
